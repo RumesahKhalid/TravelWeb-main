128230f98742baf49b5e54ad65b69276fb2bcc1c
Rumesah <https://github.com/RumesahKhalid/Travel.git>
2024-02-26 18:02:34 +0500

    change

--- src/components/Contact/contactForm.js
+++ src/components/Contact/contactForm.js
@@ -1,17 +1,94 @@
-import "./contact.css"
+import React, { useState, useRef } from "react";
+import emailjs from '@emailjs/browser';
+import "./contact.css";
 
-function ContactForm(){
-    return(
-        <div className="from-container">
-        <h1>Send a message to us!</h1>
-        <form>
-            <input placeholder="Name"/>
-            <input placeholder="Email"/>
-            <input placeholder="Subject"/>
-            <textarea placeholder="Message" rows="4"></textarea>
-            <button>Send Message</button>
-        </form>
+function ContactForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleSubjectChange = (event) => {
+    setSubject(event.target.value);
+  };
+
+  const handleMessageChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_2kxyr6r', 'template_vv6cznz', form.current, {
+        publicKey: 'shBkeBeEspvDOIYzp',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+      e.target.reset()
+  };
+
+  return (
+    <div className="form-container">
+      <h1>Contact us!</h1>
+      <form 
+      ref={form}
+      onSubmit={sendEmail}>
+        <div className="input-container">
+          <input
+            type="text"
+            value={name}
+            onChange={handleNameChange}
+            required
+          />
+          <label>Name</label>
         </div>
-    )
+        <div className="input-container">
+          <input
+            type="email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+          <label>Email</label>
+        </div>
+        <div className="input-container">
+          <input
+            type="text"
+            value={subject}
+            onChange={handleSubjectChange}
+            required
+          />
+          <label>Subject</label>
+        </div>
+        <div className="input-container">
+          <textarea
+            value={message}
+            onChange={handleMessageChange}
+            required
+          ></textarea>
+          <label>Message</label>
+        </div>
+        <button type="submit" className="btn">Send Message</button>
+      </form>
+    </div>
+  );
 }
-export default ContactForm
+
+export default ContactForm;
