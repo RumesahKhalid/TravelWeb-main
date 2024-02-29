c7ec131956b7e14bb2ec6d59bc7d955c18286011
ersa269 <ersaehsan291@gmail.com>
2024-02-29 21:02:11 +0500

    hotel

--- src/components/Contact/contactForm.js
+++ src/components/Contact/contactForm.js
@@ -1,43 +1,20 @@
-import React, { useState, useRef } from "react";
+import React, { useRef } from "react";
 import emailjs from '@emailjs/browser';
 import "./contact.css";
 
 function ContactForm() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [subject, setSubject] = useState("");
-  const [message, setMessage] = useState("");
-
-  const handleNameChange = (event) => {
-    setName(event.target.value);
-  };
-
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handleSubjectChange = (event) => {
-    setSubject(event.target.value);
-  };
-
-  const handleMessageChange = (event) => {
-    setMessage(event.target.value);
-  };
-
   const form = useRef();
 
-  function sendEmail (e) {
+  const sendEmail = (e) => {
     e.preventDefault();
-  
-    emailjs
-      .sendForm('service_2kxyr6r', 'template_vv6cznz', form.current, {
-        publicKey: 'shBkeBeEspvDOIYzp',
-      })
 
+    emailjs .sendForm('service_j9189qc', 'template_g7tza45', form.current, {
+        publicKey: 'nwBPxhYdF1amOjibf',
+      })
       .then(
-        () => {
-          console.log('SUCCESS!');
-          e.target.reset();
+        (result) => {
+          console.log('SUCCESS!', result.text);
+          console.log('Message Send');
         },
         (error) => {
           console.log('FAILED...', error.text);
@@ -45,56 +22,33 @@ function ContactForm() {
       );
   };
   
-
+  emailjs.init('nwBPxhYdF1amOjibf');
   return (
     <div className="form-container">
       <h1>Contact us!</h1>
-      <form 
-      ref={form}
-      onSubmit={sendEmail}>
+       <form  ref={form} onSubmit={sendEmail}>
         <div className="input-container">
           <input
             name="user_name"
             type="text"
-            value={inputData.user_name}
-            onChange={handleData}
             required
           />
           <label>Name</label>
         </div>
         <div className="input-container">
-          <input
-            type="email"
-            name="user_email"
-            value={inputData.user_email}
-            onChange={handleData}
-            required
-          />
-          <label>Email</label>
-        </div>
-        <div className="input-container">
-          <input
-            type="text"
-            name="subject"
-            value={inputData.subject}
-            onChange={handleData}
-            required
-          />
-          <label>Subject</label>
-        </div>
-        <div className="input-container">
+          <input type="email" name="user_email" required />
+            <label>Email</label>
+        </div>        
+          <div className="input-container">
           <textarea
             name="message"
-            value={inputData.message}
-            onChange={handleData}
             required
           ></textarea>
           <label>Message</label>
         </div>
-        <div>
-          <button type="submit" className="btn">Send Message</button>
-        </div>  
-      </form>
+        <input type="submit" value="Send" className="btn" />
+    </form>
+
     </div>
   );
 }
